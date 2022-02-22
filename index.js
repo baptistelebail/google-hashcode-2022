d93686d57703c44c0cfe636817badbe45e395834
Raphael Lecoutre <rlecoutre@liveaction.com>
2022-02-22 13:08:07 +0100

    add correct parser with customer returned

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-const { readFile } = require("./utils/parser");
+const { read } = require("./utils/utils");
 
-class Ingredient {
 
-    id;
-    dislike;
-
-    constructor(id, dislike) {
-        this.id = id;
-        this.dislike = dislike;
-    }
-}
-
-class Customer {
-    
-    likes;
-    dislikes;
-
-    constructor(likes, dislikes) {
-        this.likes = likes;
-        this.dislikes = dislikes;
-    }
-}
-
-const a = readFile('/score/a_an_example.in.txt');
-console.log(a);
+const customers = read('/score/a_an_example.in.txt');
+console.log(customers);
---
+++ utils/utils.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+
+function readFile(file) {
+  return fs.readFileSync(path.join(__dirname, `../${file}`), 'utf8');
+}
+
+class Ingredient {
+
+    id;
+    dislike;
+
+    constructor(id, dislike) {
+        this.id = id;
+        this.dislike = dislike;
+    }
+}
+
+class Customer {
+    
+    likes;
+    dislikes;
+
+    constructor(likes, dislikes) {
+        this.likes = likes;
+        this.dislikes = dislikes;
+    }
+}
+
+function read(file) {
+  const data = readFile(file);
+
+  const lines = data.split('\n');
+  lines.shift();
+
+  const customers = [];
+
+  for (let i = 0; i < lines.length / 2; i++) {
+    const likes = lines[i].split(' ');
+    likes.shift();
+
+    const dislikes = lines[i + 1].split(' ');
+    dislikes.shift();
+
+    const newCustomer = new Customer(likes, dislikes);
+
+    customers.push(newCustomer);
+  }
+
+  return customers;
+}
+
+module.exports = {
+  read,
+  Customer,
+  Ingredient,
+};
