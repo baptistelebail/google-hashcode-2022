3599d9f58eb50e405a38da4a40659c6ee3017d42
Raphael Lecoutre <rlecoutre@liveaction.com>
2022-02-24 21:30:35 +0100

    allez hop

--- index.js
+++ index.js
@@ -1,6 +1,103 @@
-const { read, generateSolution } = require("./utils/utils");
+const { result } = require("lodash");
+const { read, Contributor, Skill, Project, output } = require("./utils/utils");
 
+function findEligibleProjects(t, contributors, projects) {
+  const eligibleProjects = [];
 
-const input = read('/score/a_an_example.in.txt');
+  for (let i = 0; i < projects.length; i++) {
+    if (t <= projects[i].bestBefore - projects[i].duration) {
+      const collaboratersMatched = [];
 
-generateSolution("string");
+      projects[i].skills.forEach((skillNeeded) => {
+        contributors.forEach((c) => {
+          if (c.remainingDays === 0) {
+            c.skills.forEach((cSkill) => {
+              if (cSkill.name === skillNeeded.name && cSkill.level >= skillNeeded.level) {
+                collaboratersMatched.push(c.name);
+              }
+            });
+          }
+        });
+      });
+
+      if (collaboratersMatched.length === projects[i].skills.length) {
+        eligibleProjects.push({
+          project: projects[i],
+          contributors: collaboratersMatched
+        });
+      }
+    }
+  }
+
+  eligibleProjects.sort((eligibleProject1, eligibleProject2) =>
+    eligibleProject1.project.score - eligibleProject2.project.score
+  );
+
+  return eligibleProjects;
+}
+
+function findProjectContributors(project, contributors) {
+  const availableContributors = contributors;
+}
+
+const files = [
+  {
+    input: '/score/a_an_example.in.txt',
+    output: '../outputs/a_an_example.in.txt'
+  },
+  {
+    input: '/score/b_better_start_small.in.txt',
+    output: '../outputs/b_better_start_small.in.txt'
+  },
+  {
+    input: '/score/c_collaboration.in.txt',
+    output: '../outputs/c_collaboration.in.txt'
+  },
+  {
+    input: '/score/d_dense_schedule.in.txt',
+    output: '../outputs/d_dense_schedule.in.txt'
+  },
+  {
+    input: '/score/e_exceptional_skills.in.txt',
+    output: '../outputs/e_exceptional_skills.in.txt'
+  },
+  {
+    input: '/score/f_find_great_mentors.in.txt',
+    output: '../outputs/f_find_great_mentors.in.txt'
+  },
+];
+
+files.forEach(({ input, output: outputName }) => {
+  let [contributors, projects] = read(input);
+
+  const projectsDone = [];
+
+  let day = 0;
+
+  let projectRemainingDays = 0;
+
+  while (true) {
+    if (projectRemainingDays === 0) {
+      const eligiblesProject = findEligibleProjects(day, contributors, projects);
+
+      if (eligiblesProject.length === 0) {
+        break;
+      }
+
+      const projectOfTheDay = eligiblesProject.shift();
+
+      projectRemainingDays = projectOfTheDay.project.duration;
+
+      projectsDone.push(projectOfTheDay);
+      
+      const projectIndex = projects.findIndex((project) => project.name === projectOfTheDay.project.name);
+      projects.splice(projectIndex, 1);
+    } else {
+      projectRemainingDays = projectRemainingDays - 1;
+    }
+
+    day = day + 1;
+  }
+
+  output(projectsDone, outputName);
+})
--- utils/utils.js
+++ utils/utils.js
@@ -5,46 +5,110 @@ function readFile(file) {
   return fs.readFileSync(path.join(__dirname, `../${file}`), 'utf8');
 }
 
-class Customer {
-    
-    likes;
-    dislikes;
+class Skill {
+  name;
+  level;
 
-    constructor(likes, dislikes) {
-        this.likes = likes;
-        this.dislikes = dislikes;
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
+  }
+}
+
+class Contributor {
+    name;
+    skills;
+    remainingDays;
+
+    constructor(name, skills) {
+        this.name = name;
+        this.skills = skills;
+        this.remainingDays = 0;
+    }
+
+    setRemainingDays(duration) {
+      this.remainingDays = duration;
+    }
+
+    decrementRemainingDays() {
+      this.remainingDays = this.remainingDays - 1;
+    }
+}
+
+class Project {
+    name;
+    skills;
+    score;
+    duration;
+    bestBefore;
+
+    constructor(name, skills, score, duration, bestBefore) {
+        this.name = name;
+        this.skills = skills;
+        this.score = score;
+        this.duration = duration;
+        this.bestBefore = bestBefore;
     }
 }
 
 function read(file) {
   const data = readFile(file);
 
+  const contributors = [];
+  const projects = [];
+
   const lines = data.split('\n');
-  lines.shift();
+  const [contributorsNumber, projectsNumber] = lines.shift().split(' ');
 
-  const customers = [];
+  for (let i = 0; i < contributorsNumber; i = i + 1) {
+    let [name, skillsNumber] = lines.shift().split(' ');
+    skillsNumber = parseInt(skillsNumber);
 
-  for (let i = 0; i < lines.length; i = i + 2) {
-    const likes = lines[i].split(' ');
-    likes.shift();
+    const skills = [];
 
-    const dislikes = lines[i + 1].split(' ');
-    dislikes.shift();
+    for (let j = 0; j < skillsNumber; j++) {
+      const [skillName, skillLevel] = lines.shift().split(' ');
+      skills.push(new Skill(skillName, skillLevel));
+    }
 
-    const newCustomer = new Customer(likes, dislikes);
-
-    customers.push(newCustomer);
+    contributors.push(new Contributor(name, skills));
   }
 
-  return customers;
+  for (let i = 0; i < projectsNumber; i = i + 1) {
+    const [name, duration, score, bestBefore, skillsNumber] = lines.shift().split(' ');
+
+    const skills = [];
+
+    for (let j = 0; j < skillsNumber; j = j + 1) {
+      const [skillName, level] = lines.shift().split(' ');
+      skills.push(new Skill(skillName, level));
+    }
+
+    projects.push(new Project(name, skills, score, duration, bestBefore));
+  }
+
+  return [contributors, projects];
 }
 
-function generateSolution(data) {
-  fs.writeFileSync(path.join(__dirname, '../result.txt'), data);
-  console.log('\n\nResult file generated\n\n');
+function output(projectsDone, fileName) {
+  
+  let outputString = `${projectsDone.length}\n`;
+
+  projectsDone.forEach((projectDone) => {
+    outputString = `${outputString}${projectDone.project.name}\n`;
+
+    let contributors = projectDone.contributors.join(' ');
+
+    outputString = `${outputString}${contributors}\n`;
+  });
+
+  fs.writeFileSync(path.join(__dirname, fileName), outputString);
 }
 
 module.exports = {
   read,
-  generateSolution
+  Contributor,
+  Skill,
+  Project,
+  output,
 };
